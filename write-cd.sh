a8ab777da6ffbd6739b0b6e0b582d042e8b8b0ff
Michael Madden <mikemadden42@icloud.com>
2024-02-15 07:10:39 -0600

    Fix shellcheck issues.

--- write-cd.sh
+++ write-cd.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ "x$1" = "x" ]; then
+if [ "$1" = "" ]; then
 	echo "usage: $0 <filename.toc>"
 	exit 1
 fi
 
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
 	echo "Error: file $1 does not exist."
 fi
 
-cdrdao write --eject --device /dev/sr0 --driver generic-mmc-raw $1
+cdrdao write --eject --device /dev/sr0 --driver generic-mmc-raw "$1"
