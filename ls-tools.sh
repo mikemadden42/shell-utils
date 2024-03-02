2e7c60d2b3b9a3740e5729ae99f7ba2f63c257f9
Michael Madden <mikemadden42@icloud.com>
2024-03-01 20:19:21 -0600

    First common-commands.sh & ls-tools.sh.

---
+++ ls-tools.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+dpkg -l \
+	acpi \
+	apt \
+	aptitude \
+	bash \
+	code \
+	coreutils \
+	diffutils \
+	dpkg \
+	findutils \
+	git \
+	google-chrome-stable \
+	grep \
+	htop \
+	mount \
+	python3 \
+	ripgrep \
+	rsync \
+	screen \
+	silversearcher-ag \
+	slack-desktop \
+	sudo \
+	vim \
+	zoom \
+	zsh
