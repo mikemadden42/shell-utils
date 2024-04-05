2f38aa774a849cc9b1477632199f6dc4ef280698
Michael Madden <mikemadden42@icloud.com>
2024-04-04 19:25:03 -0500

    Add progress to clean-go.sh.

--- clean-go.sh
+++ clean-go.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Cleaning cache..."
 go clean -cache
+echo "Cleaning modcache..."
 go clean -modcache
+echo "Cleaning testcache..."
 go clean -testcache
+echo "Cleaning fuzzcache..."
 go clean -fuzzcache
