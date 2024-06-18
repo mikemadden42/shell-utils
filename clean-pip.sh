d089677d2bc3fea4015261a34b16807c985b90d4
Michael Madden <mikemadden42@icloud.com>
2024-06-18 06:11:55 -0500

    Initial clean-pip.sh.

---
+++ clean-pip.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "pip cache info..."
+pip cache info
+echo "pip cache list..."
+pip cache list
+echo "pip cache purge..."
+pip cache purge
+echo "pip cache info..."
+pip cache info
